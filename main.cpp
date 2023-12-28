1afd21ac53d6cd59450a3b1226dca6530d1d9712
Overionised <153439700+Overionised@users.noreply.github.com>
2023-12-28 16:02:39 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "monsters.cpp"
+
+using namespace std;
+
+
+
+
+int main() {
+
+  string monster;
+  cin>>monster;
+
+  cout<<"OH NO a wild "<<monster<<" appeared!"<<endl;
+  fojtis_good();
+  
+}
